2f58fe79223e0ff5cad5ee1ed9bb91fb68446b9c
Harvey Yerik <yerik05vh@gmail.com>
2023-03-15 21:44:31 -0500

    dev: update

--- lib/database/db_admin.dart
+++ lib/database/db_admin.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
 class DBAdmin{
-  initDataBase(){
+  initDataBase() async {
     print("Iniciando db");
-    
+    // Con directory.path, capturamos el cirectorio de nuestra aplicación.
+    // Con join, unimos path, en este caso creamos un archivo DBBooks.db
+    Directory directory = await getApplicationDocumentsDirectory();
+    String pathDataBase = join(directory.path, "DBBooks.db");
   }
 }

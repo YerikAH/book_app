cbc9efe6692a25d8d085c18e498df991863e1e3f
Harvey Yerik <yerik05vh@gmail.com>
2023-03-16 22:13:49 -0500

    dev: create data base

--- lib/database/db_admin.dart
+++ lib/database/db_admin.dart
@@ -1,13 +1,22 @@
 import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 class DBAdmin{
-  initDataBase() async {
-    print("Iniciando db");
     // Con directory.path, capturamos el cirectorio de nuestra aplicación.
     // Con join, unimos path, en este caso creamos un archivo DBBooks.db
+  initDataBase() async {
     Directory directory = await getApplicationDocumentsDirectory();
     String pathDataBase = join(directory.path, "DBBooks.db");
+    return await openDatabase(
+      pathDataBase,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          "CREATE TABLE BOOK(id INTEGER PRIMARY KEY AUTOINCREMENT , title TEXT, author TEXT, description TEXT, image TEXT)"
+        );
+      }
+    );
   }
 }

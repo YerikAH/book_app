5f66c6a4dbf84a084a4b665580b51881b86a26fc
Harvey Yerik <yerik05vh@gmail.com>
2023-03-16 22:23:28 -0500

    dev: update project

--- lib/database/db_admin.dart
+++ lib/database/db_admin.dart
@@ -6,6 +6,8 @@ import 'package:sqflite/sqflite.dart';
 class DBAdmin{
     // Con directory.path, capturamos el cirectorio de nuestra aplicación.
     // Con join, unimos path, en este caso creamos un archivo DBBooks.db
+    // Ya no es necesario poner CREATE DATABASE, ya que se crea cuando creamos el archivo
+    // openDataBase, es para abrir nuestra base de datos, recibe el path, la versión de nuestra base de datos, y onCreate, se ejecuta cuando se crea una nueva base de datos. En esta función se pueden crear las tablas y establecer los índices necesarios para la base de datos.
   initDataBase() async {
     Directory directory = await getApplicationDocumentsDirectory();
     String pathDataBase = join(directory.path, "DBBooks.db");
